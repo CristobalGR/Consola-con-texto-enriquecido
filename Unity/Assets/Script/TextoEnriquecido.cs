c8689756e4fd9e581356d1e7e4018b21bf00d0c4
CristobalGR <cristobalgutierrez@ceitartessos.com>
2019-10-27 12:15:09 +0100

    Trabajo

---
+++ Unity/Assets/Script/TextoEnriquecido.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextoEnriquecido : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ("Este es un ejemplo de texto enriquecido <b>con negrita.</b>");
+		Debug.Log ("Este es un ejemplo de texto <b><i>con negrita y cursiva.</i></b>");
+		Debug.Log ("Este es un ejemplo de texto <i><color=red>con cursiva y en rojo.</color></i>");
+		Debug.Log ("Este es un ejemplo de texto <color=red><b> con negrita y en rojo.</b></color>");
+		Debug.Log ("Este es un ejemplo de texto <color=green><size=20>en verde y a 20 pixeles</size></color>");
+	}
+}
